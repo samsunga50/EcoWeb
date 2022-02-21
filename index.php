c0adcdc2c40d70e9595261bca93f7aadf18c3271
samsunga50 <samsunga50.in.2020@gmail.com>
2022-02-21 14:20:49 +0400

    php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    require_once('./inc/config.php');    
+    require_once('./inc/helpers.php');  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (isset($metaDesc)?$metaDesc:'EcoWeb')?>">
+  
+    <title><?php echo (isset($pageTitle)?$pageTitle:'PHP Shopping Cart')?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="images/cat1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="images/category-2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100" src="images/category-3.jpg" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
+</body>
+</html>
---
+++ product2.php
@@ -0,0 +1,67 @@
+<?php 
+    session_start();
+    require_once('./inc/config.php');    
+    require_once('./inc/helpers.php');  
+
+    $sql = "SELECT p.*,pdi.img  
+            FROM products p
+            INNER JOIN product_images pdi ON pdi.product_id = p.id
+            WHERE pdi.is_featured = 1
+            LIMIT 12,13";
+    $handle = $db->prepare($sql);
+    $handle->execute();
+    $getAllProducts = $handle->fetchAll(PDO::FETCH_ASSOC);
+
+    $pageTitle = 'EcoWeb | Products';
+	$metaDesc = 'Product Page';
+
+    include('layouts/header.php');
+?>
+    <div class="row"> 
+        <?php
+        foreach($getAllProducts as $product)
+        {
+            $imgUrl = PRODUCT_IMG_URL.str_replace(' ','-',strtolower($product['product_name']))."/".$product['img'];
+        ?>
+            <div class="col-md-3  mt-2">
+                <div class="card">
+                     <a href="single-product.php?product=<?php echo $product['id']?>">
+                        <img class="card-img-top" src="<?php echo $imgUrl?>" alt="<?php echo $product['product_name'] ?>" style = "width: 100%">
+                    </a>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="single-product.php?product=<?php echo $product['id'] ?>">
+                                <?php echo $product['product_name']; ?>
+                            </a>
+                        </h5>
+                        <strong>RS <?php echo $product['price']?></strong>
+
+                        <p class="card-t">
+                            <?php echo substr($product['short_description'],0,50) ?>'...
+                        </p>
+                        <p class="card-text">
+                            <a href="single-product.php?product=<?php echo $product['id']?>" class="btn btn-primary btn-sm">
+                                View
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        <?php 
+        }
+        ?>
+
+        <nav aria-label="...">
+            <ul class="pagination">
+                <li> <a class="page-link" href="product.php" tabindex="-1">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="product.php">1</a></li>
+                <li class="page-item active">
+                    <a class="page-link" href="product2.php">2 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="page-item disabled">
+                <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+<?php include('layouts/footer.php');?>
